26499797af89c35d3674b0a2e792b2336bc79e92
AbdulRahim <rahimshaikh7000@gmail.com>
2018-11-04 18:12:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
-    var name=prompt(" Hi! What's your Good Name");
-    alert(name+" " +"Happy Diwali to you and your Family");
+    var name=prompt(" Hi!! What's your Good Name.");
+    alert(name+" " +" Happy Diwali to you and your Family!");
   
       
     
